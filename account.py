be186ab73465454a7f5447ef1420ee6c6c57785c
thuitafaith <thuitamuthoni15@gmail.com>
2018-04-03 15:00:10 +0300

    5 tests ran for creating of an account

--- account.py
+++ account.py
@@ -2,8 +2,19 @@ class Account:
     '''
     class that generates new instances of accounts
     '''
+    account_list = [] #Empty account list
+    def save_account(self):
 
+         Account.account_list.append(self)
+    def delete_account(self):
 
+        Account.account_list.remove(self)
+
+    @classmethod
+    def find_by_email(cls,email):
+        for account in cls.account_list:
+            if account.email == email:
+                return account
     def __init__(self,first_name,last_name,email,password):
 
         self.first_name = first_name
---
+++ account_test.py
@@ -0,0 +1,54 @@
+import unittest #importing the unittest module
+from account import Account #Importing the accountclass
+
+class TestAccount(unittest.TestCase):
+     """
+     Test class that defines test cases for the contact class behaviours.
+     Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+     """
+
+     def setUp(self):
+         '''
+         set up method run before each test cases.
+         '''
+         self.new_account = Account("Faith","Thuita","thuita15@gmail.com","egui") #create account object
+     def test_init(self):
+         '''
+         test_init case to test if the object is initialized properly
+         '''
+         self.assertEqual(self.new_account.first_name,"Faith")
+         self.assertEqual(self.new_account.last_name,"Thuita")
+         self.assertEqual(self.new_account.email,"thuita15@gmail.com")
+         self.assertEqual(self.new_account.password,"egui")
+
+     def test_save_account(self):
+
+         self.new_account.save_account() #saving the new account
+
+         self.assertEqual(len(Account.account_list),1)
+     def tearDown(self):
+         Account.account_list = []
+
+     def test_save_multiple_account(self):
+
+         self.new_account.save_account()
+         test_account = Account("mwangi","njuguna","mwangi12@gmail.com","press") #new account
+         test_account.save_account()
+         self.assertEqual(len(Account.account_list),2)
+
+     def test_delete_account(self):
+         self.new_account.save_account()
+         test_account = Account("mwangi","njuguna","mwangi12@gmail.com","press")
+         test_account.save_account()
+
+         self.new_account.delete_account() #deleting account
+         self.assertEqual(len(Account.account_list),1)
+     def test_find_account_by_email(self):
+         self.new_account.save_account()
+         test_account = Account("mwangi","njuguna","mwangi12@gmail.com","press")
+         test_account.save_account()
+         found_account = Account.find_by_email("mwangi12@gmail.com")
+         self.assertEqual(found_account.email,test_account.email)
+if __name__ == '__main__':
+    unittest.main()
