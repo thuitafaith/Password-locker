b071cbd7d8f02dcea7c4bd31bc7f601d934d063a
thuitafaith <thuitamuthoni15@gmail.com>
2018-04-03 16:29:58 +0300

    file for profiles tests created

---
+++ profiles.py
@@ -0,0 +1,11 @@
+class Profiles:
+    '''
+    create instances of class Profiles
+    '''
+
+
+def __init__(self,username,email,password):
+
+    self.username = username
+    self.email = email
+    self.password = password
